5c2b6790804491a0b1c81d63e76e1a2484d59d2a
PRIYANSHU HISSARIA <48879804+Priyanshuhissaria@users.noreply.github.com>
2019-10-20 18:59:41 +0530

    added single player mode

--- README.py
+++ README.py
@@ -1,27 +1,35 @@
-# stone_paper_scissors
-print("..stone...")
-print("...paper...")
-print("...scissors...")
+from random import randint
+print("stone...")
+print("Paper...")
+print("Scissors...")
 
-player1 = input("(enter Player 1's choice): ")
-print("***NO CHEATING!\n\n" * 20)
-player2 = input("(enter Player 2's choice): ")
-
-print("SHOOT!")
-
-if player1 == "stone" and player2 == "scissors":
-	print("player1 wins!")
-elif player1 == "stone" and player2 == "paper":
-	print("player2 wins!")
-elif player1 == "paper" and player2 == "stone":
-	print("player1 wins!")
-elif player1 == "paper" and player2 == "scissors":
-	print("player2 wins!")
-elif player1 == "scissors" and player2 == "stone":
-	print("player2 wins!")
-elif player1 == "scissors" and player2 == "paper":
-	print("player1 wins!")
-elif player1 == player2:
-	print("It's a tie!")
+player = input("Player, make your move: ").lower()
+rand_num = randint(0,2)
+if rand_num == 0:
+	computer = "stone"
+elif rand_num == 1:
+	computer = "paper"
 else:
-	print("something went wrong")
+	computer = "scissors"
+
+print(f"Computer plays {computer}" )
+
+if player == computer:
+	print("It's a tie!")
+elif player == "stone":
+	if computer == "scissors":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "paper":
+	if computer == "stone":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "scissors":
+	if computer == "paper":
+		print("player wins!")
+	else:
+		print("computer wins!")	
+else:
+	print("Please enter a valid move!")
