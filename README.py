ee960ab9055c8632bb24b980a794eecf10a09d03
PRIYANSHU HISSARIA <48879804+Priyanshuhissaria@users.noreply.github.com>
2019-10-20 18:47:59 +0530

    starting of the game between two players

--- README.py
+++ README.py
@@ -8,3 +8,19 @@ player2 = input("(enter Player 2's choice): ")
 
 print("SHOOT!")
 
+if player1 == "stone" and player2 == "scissors":
+	print("player1 wins!")
+elif player1 == "stone" and player2 == "paper":
+	print("player2 wins!")
+elif player1 == "paper" and player2 == "stone":
+	print("player1 wins!")
+elif player1 == "paper" and player2 == "scissors":
+	print("player2 wins!")
+elif player1 == "scissors" and player2 == "stone":
+	print("player2 wins!")
+elif player1 == "scissors" and player2 == "paper":
+	print("player1 wins!")
+elif player1 == player2:
+	print("It's a tie!")
+else:
+	print("something went wrong")
